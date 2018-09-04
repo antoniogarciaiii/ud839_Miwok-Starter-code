768978ea21c2182aa7a40abdb9dde84f1fd6e7e2
antoniogarciaiii <antoniogarciaiii@gmail.com>
2018-09-04 14:54:20 -0500

    finished Lesson 3: arrays, lists, loops, & Custom Classes concept 4: Quiz: add words to NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -9,5 +9,20 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //TODO: add array of words here
+        // Source: http://www.dmc.fmph.uniba.sk/public_html/doc/Java/ch5.htm#DeclaringArrayVariables
+        String[] words = new String[10];
+        words[0] = "One";
+        words[1] = "Two";
+        words[2] = "Three";
+        words[3] = "Four";
+        words[4] = "Five";
+        words[5] = "Six";
+        words[6] = "Seven";
+        words[7] = "Eight";
+        words[8] = "Nine";
+        words[9] = "Ten";
+
     }
 }
