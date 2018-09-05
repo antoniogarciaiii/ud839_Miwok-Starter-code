77f7d1e3c420d4fc9e5a56d043fc22c93528f8ae
antoniogarciaiii <antoniogarciaiii@gmail.com>
2018-09-05 16:01:40 -0500

    finished Lesson 3: arrays, lists, loops, & Custom Classes concept 5: Quiz: Verify Array Elements with Log Messages

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -23,6 +24,18 @@ public class NumbersActivity extends AppCompatActivity {
         words[7] = "Eight";
         words[8] = "Nine";
         words[9] = "Ten";
+
+        Log.v("Numbersactivity", "Word at index 0: " + words[0]);
+        Log.v("Numbersactivity", "Word at index 1: " + words[1]);
+        Log.v("Numbersactivity", "Word at index 2: " + words[2]);
+        Log.v("Numbersactivity", "Word at index 3: " + words[3]);
+        Log.v("Numbersactivity", "Word at index 4: " + words[4]);
+        Log.v("Numbersactivity", "Word at index 5: " + words[5]);
+        Log.v("Numbersactivity", "Word at index 6: " + words[6]);
+        Log.v("Numbersactivity", "Word at index 7: " + words[7]);
+        Log.v("Numbersactivity", "Word at index 8: " + words[8]);
+        Log.v("Numbersactivity", "Word at index 9: " + words[9]);
+
 
     }
 }
